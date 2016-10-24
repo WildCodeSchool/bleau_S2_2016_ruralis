cc94c95e5afe391ac80dd438bd6ce34ea0ae8804
Florian Grandjean <florian.pdf@gmail.com>
2016-10-24 15:57:33 +0200

    add favicon

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="img/logo.png" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
     <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
