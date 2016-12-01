3f384e9933477308d6a8be9a733393d0508f02a9
mariellelau <marielle.lautrou@gmail.com>
2016-12-01 01:06:37 +0100

    modifications de la partie admin

--- src/RuralisBundle/Form/ArticleType.php
+++ src/RuralisBundle/Form/ArticleType.php
@@ -5,6 +5,10 @@ namespace RuralisBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ArticleType extends AbstractType
 {
@@ -13,7 +17,31 @@ class ArticleType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nom')->add('descriptif')->add('contenu')->add('auteur')->add('date')        ;
+        $builder
+            ->add('typeAffichage', EntityType::class, array(
+                'class' => 'RuralisBundle:TypeAffichage',
+                'choice_label' => 'nom',
+                'placeholder' => 'Selectionnez un type',
+                'required' => true
+            ))
+
+            ->add('nom')
+            ->add('descriptif')
+            ->add('contenu')
+            ->add('auteur')
+            ->add('date', DateType::class, array(
+                'widget' => 'single_text',
+                'input' => 'datetime',
+                // do not render as type="date", to avoid HTML5 date pickers
+                'html5' => false,
+                'format' => 'yyyy-MM-dd',
+                // add a class that can be selected in JavaScript
+                'attr' => ['class' => 'js-datepicker'],
+            ))
+            ->add('image', FileType::class, array(
+        'label' => 'image à télécharger : ',
+        'data_class' => null
+    ));
     }
     
     /**
