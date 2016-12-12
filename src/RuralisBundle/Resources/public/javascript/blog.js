a7745279c0f101b3d6e4771953afa77c47204daa
mariellelau <marielle.lautrou@gmail.com>
2016-12-12 10:37:08 +0100

    ajout du dossier JS

---
+++ src/RuralisBundle/Resources/public/javascript/blog.js
@@ -0,0 +1,9 @@
+$('.datepicker').pickadate({
+//            selectMonths: true, // Creates a dropdown to control month
+//            selectYears: 15 // Creates a dropdown of 15 years to control year
+    format: 'dd mmmm yyyy',
+    formatSubmit: 'mm/dd/yyyy'
+});
+$(document).ready(function() {
+    $('select').material_select();
+});
