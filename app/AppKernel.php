4c967beb824e302bac94744b1ecf639ad00631fa
mariellelau <marielle.lautrou@gmail.com>
2017-01-15 11:12:37 +0100

    ajout de FOSUser

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new RuralisBundle\RuralisBundle(),
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
